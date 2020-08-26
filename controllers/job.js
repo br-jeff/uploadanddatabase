c00488e4f57e7bc9092092fc042f59b55eaf6262
kalavh <jefferson_1996Th@hotmail.com>
2020-08-25 23:45:24 -0300

    body parser

--- controllers/job.js
+++ controllers/job.js
@@ -1,5 +1,9 @@
 module.exports = app => {
 
     app.get('/jobs',(req,res)=> res.send('jobs route'))
+    app.post('/jobs', (req,res) => {
+        console.log(req.body)
+        res.send('post')
 
+    })
 }
