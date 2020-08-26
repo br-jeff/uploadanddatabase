67bf87c6af9b967ad42aba8456a014ae94b09a8e
kalavh <jefferson_1996Th@hotmail.com>
2020-08-26 00:31:51 -0300

    now we are on database

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const customExpress = require("./config/customExpress")
+const connetion = require('./infrastructure/connection')
+const connection = require("./infrastructure/connection")
 const app = customExpress()
+
+connection.connect(err => {
+
+    if(err) {
+        console.log(err)
+    } else {
+        console.log('you are connected')
+    }
+  
+})
 
 app.listen(3000)
