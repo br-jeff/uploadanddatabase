0f97bbffd411b38d6f6d7dd20c79f0e30a1858c3
kalavh <jefferson_1996Th@hotmail.com>
2020-08-26 00:45:04 -0300

    table job was create

--- infrastructure/Tables.js
+++ infrastructure/Tables.js
@@ -3,7 +3,25 @@ const { connect } = require("./connection")
 
 class Tables {
     init(connection) {
-        console.log('teste tables')
+        this.connection = connection
+        this.makeJob()
+    }
+
+        
+    makeJob() {
+        const sql = 
+        `create table job (id int NOT null AUTO_INCREMENT,
+        client varchar(50) NOT null, pet varchar(20),
+        service varchar(20) NOT null, status varchar(20) NOT null,
+        obs text , PRIMARY KEY(id)  )`
+
+        this.connection.query(sql, (err,)=> {
+            if (err) { console.log(err)} 
+            else{
+                console.log('table job was create')
+            }
+
+        })
     }
 }
 
